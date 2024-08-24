1f1a7ec651c3cb8456abf84380fd7f5ac4042d84
Chamalfernando <chamaldeshitha2001@gmail.com>
2024-08-24 12:13:44 +0530

    commit before pull

--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -5,15 +5,18 @@
 // view all requests
 // remove unethical recipients
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const AdminController = require("../Controllers/Home/AdminController");
+const AdminController = require("../Controllers/Admin/AdminController");
 
-router.get('/allusers', AdminController.getAllUsers);
-router.get('/alldonations', AdminController.getAllDonations);
-router.get('/alltokens', AdminController.getAllTokens);
-router.get('/allbeneficiaries', AdminController.getAllBeneficiaries);
-router.get('/allrequests', AdminController.getAllRequests);
-router.delete('/removeuser/:id', AdminController.removeUser);
+router.get("/allusers", AdminController.getAllUsers);
+router.get("/alldonations", AdminController.getAllDonations);
+router.get("/alltokens", AdminController.getAllTokens);
+router.get("/allbeneficiaries", AdminController.getAllBeneficiaries);
+router.get("/allrequests", AdminController.getAllRequests);
+router.delete("/removeuser/:id", AdminController.removeUser);
 
-module.exports = router
+console.log(AdminController.getAllUsers); // Should not be undefined
+console.log(AdminController.getAllDonations); // Should not be undefined
+
+module.exports = router;
